45d0294ac547d549d5d0857be624c6b6e46b8597
mocavada <marc@awesomewebsite.ca>
2021-05-20 22:16:04 -0400

    Module3-Post data

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,7 @@
 import { addnewProduct } from '../controllers/controllers';
 
-
 const routes = (app) => {
-    app.route('./products')
-    // Post endpoint
-    .post(addnewProduct);
+    app.route('/products').post(addnewProduct);
 }
 
 export default routes;
