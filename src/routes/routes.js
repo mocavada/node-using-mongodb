0f6c3690be6723b933c213cbf3b2ffc0416b9da2
mocavada <marc@awesomewebsite.ca>
2021-05-20 22:34:02 -0400

    Module4-GetByID

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,12 @@
-import { addnewProduct } from '../controllers/controllers';
+import { addnewProduct, getProducts, getProductWithID } from '../controllers/controllers';
 
 const routes = (app) => {
-    app.route('/products').post(addnewProduct);
+    app.route('/products')
+    .get(getProducts)
+    .post(addnewProduct);
+   
+    app.route('/products/:ProductID')
+    .get(getProductWithID);
 }
 
 export default routes;
